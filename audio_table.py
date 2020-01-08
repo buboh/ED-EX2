c2e8de7265a02032d24c020ca937979fad1c958c
konstid <59444006+konstid@users.noreply.github.com>
2020-01-08 11:30:44 +0100

    Add files via upload

---
+++ audio_table.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import arff
+
+
+#load average audio data and merge with ‚goodforairplane‘ attribute
+
+data_dev = pd.read_excel('data/CoE_dataset/Dev_Set/dev_set_groundtruth_and_trailers.xls').set_index('movie')
+data_test = pd.read_csv('data/CoE_dataset/Test_Set/test_set_inclGoodforairplane.csv').set_index('movie')
+
+indices = ['movie','MFCC_1', 'MFCC_2', 'MFCC_3', 'MFCC_4', 'MFCC_5',
+           'MFCC_6', 'MFCC_7', 'MFCC_8', 'MFCC_9', 'MFCC_10', 'MFCC_11', 'MFCC_12', 'MFCC_13']
+
+def audio_selection(file_name, set_selection):
+    try:
+        if set_selection == 'test':
+            my_data = np.genfromtxt('data/CoE_dataset/Test_Set/audio_avg/'+ file_name + '.csv', delimiter=',')
+        elif set_selection == 'dev':
+            my_data = np.genfromtxt('data/CoE_dataset/Dev_Set/audio_avg/'+ file_name + '.csv', delimiter=',')
+        else: 
+            return np.full((13), np.nan)
+    except:
+        return np.full((13), np.nan)
+    return my_data[:,1]
+
+#function below loads audio data and merges with attribute 'goodforairplane'
+#set_selection has to be 'dev' or 'test'
+
+def audio_data(df, set_selection):
+    titles = df.index.tolist()
+    split_data = pd.DataFrame(columns = indices).set_index('movie')
+    for title in titles:
+        split_data.loc[title] = audio_selection(df.loc[title]['filename'], set_selection)
+    split_data = split_data.dropna()
+    
+    if set_selection == 'test':
+        return(pd.concat([split_data, data_test['goodforairplane']], axis=1, sort=True).reindex(split_data.index))
+    
+    if set_selection == 'dev':
+        return(pd.concat([split_data, data_dev['goodforairplane']], axis=1, sort=True).reindex(split_data.index))
+    
+    return(split_data)
+
+# test data import
+dev_data_audio = audio_data(data_dev, 'dev')
+
+# dev data import
+test_data_audio = audio_data(data_test, 'test')
---
+++ visual_table_arff_export.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import arff
+# Unzip CoE_dataset_offical_release.ip in 'working_directory/data/..' (I had to to save the xls as csv)
+
+# Created test.csv by merging test_set_trailers.xls with dataset_complete.xlsx (tab test)
+data_dev = pd.read_excel('data/CoE_dataset/Dev_Set/dev_set_groundtruth_and_trailers.xls').set_index('movie')
+data_test = pd.read_csv('data/CoE_dataset/Test_Set/test_set_inclGoodforairplane.csv').set_index('movie')
+
+
+# csv below was extraced from 'data/mediaeval/dataset_complete.xlsx' tab test
+#data_test = pd.read_csv('data/CoE_dataset/test_set_trailers.csv').set_index('movie')
+
+
+
+columns_avg = ['movie']
+for i in range(826):
+    columns_avg.append('visual_'+ str(i+1))
+columns_all = columns_avg.copy()
+
+for i in range(826):
+    columns_all.append('visual_'+ str(i+827))
+
+# in case we have to ommit NaN values for the mean -> np.nanmean()
+
+def visual_mean(file_name, set_selection):
+    try:
+        if set_selection == 'test':
+            my_data = np.genfromtxt('data/CoE_dataset/Test_Set/vis_descriptors/'+ file_name + '.csv', delimiter=',')
+            vis = (my_data[0]+my_data[1])/2
+        elif set_selection == 'dev':
+            my_data = np.genfromtxt('data/CoE_dataset/Dev_Set/vis_descriptors/'+ file_name + '.csv', delimiter=',')
+            vis = (my_data[0]+my_data[1])/2
+        else: 
+            vis = np.full((826), np.nan)
+    except:
+        vis = np.full((826), np.nan)
+    return vis
+
+def visual_all(file_name, set_selection):
+    try:
+        if set_selection == 'test':
+            my_data = np.genfromtxt('data/CoE_dataset/Test_Set/vis_descriptors/'+ file_name + '.csv', delimiter=',')
+            vis = np.concatenate((my_data[0], my_data[1]))
+        elif set_selection == 'dev':
+            my_data = np.genfromtxt('data/CoE_dataset/Dev_Set/vis_descriptors/'+ file_name + '.csv', delimiter=',')
+            vis = np.concatenate((my_data[0], my_data[1]))
+        else: 
+            vis = np.full((826*2), np.nan)
+    except:
+        vis = np.full((826*2), np.nan)
+    return vis
+
+def visual_data(df, columns_, set_selection):
+    titles = df.index.tolist()
+    
+    split_data = pd.DataFrame(columns = columns_).set_index('movie')
+    
+    if len(columns_) == 827:
+        for title in titles:
+            try:
+                split_data.loc[title] = visual_mean(df.loc[title]['filename'], set_selection)
+            except:
+                continue
+                
+    if len(columns_) == 1653:
+        for title in titles:
+            try:
+                split_data.loc[title] = visual_all(df.loc[title]['filename'], set_selection)
+            except:
+                continue
+    split_data = split_data.dropna()
+    return(split_data)
+
+vis_dev = visual_data(data_dev, columns_all, 'dev')
+vis_dev_avg = visual_data(data_dev, columns_avg, 'dev')
+
+vis_test = visual_data(data_test, columns_all, 'test')
+vis_test_avg = visual_data(data_test, columns_avg, 'test')
+
+
+arff.dump('data/WEKA_files/visual_files/dev_data_vis.arff', vis_dev.values
+      , relation='visual_descriptors'
+      , names=vis_dev.columns)
+
+arff.dump('data/WEKA_files/visual_files/dev_data_vis_avg.arff', vis_dev_avg.values
+      , relation='visual_descriptors'
+      , names=vis_dev_avg.columns)
+
+arff.dump('data/WEKA_files/visual_files/test_data_vis.arff', vis_test.values
+      , relation= 'visual_descriptors'
+      , names=vis_test.columns)
+
+arff.dump('data/WEKA_files/visual_files/test_data_vis_avg.arff', vis_test_avg.values
+      , relation= 'visual_descriptors'
+      , names=vis_test_avg.columns)

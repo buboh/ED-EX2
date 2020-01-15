0f38cd0ec9dc85ac0171e45ca42f0624d7a5eafd
konstid <59444006+konstid@users.noreply.github.com>
2020-01-15 11:19:02 +0100

    commit

---
+++ combined_arff_export.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+import arff
+# Unzip CoE_dataset_offical_release.ip in 'working_directory/data/..' (I had to to save the xls as csv)
+
+# Created test.csv by merging test_set_trailers.xls with dataset_complete.xlsx (tab test)
+data_dev = pd.read_excel('data/CoE_dataset/Dev_Set/dev_set_groundtruth_and_trailers.xls').set_index('movie')
+data_test = pd.read_csv('data/CoE_dataset/Test_Set/test_set_inclGoodforairplane.csv').set_index('movie')
+
+
+
+meta_dev = pd.read_csv('data/csv_files/metadata_files/dev_data_meta.csv').set_index('Unnamed: 0')
+meta_test = pd.read_csv('data/csv_files/metadata_files/test_data_meta.csv').set_index('Unnamed: 0')
+meta_dev.columns = meta_dev.columns.str.replace(' ', '_')
+meta_test.columns = meta_test.columns.str.replace(' ', '_')
+rating_dev = pd.read_csv('data/csv_files/user_rating_files/dev_data_rating.csv').set_index('Unnamed: 0')
+rating_test = pd.read_csv('data/csv_files/user_rating_files/test_data_rating.csv').set_index('Unnamed: 0')
+
+visual_dev = pd.read_csv('data/csv_files/visual_files/dev_data_visual.csv').set_index('movie')
+visual_test = pd.read_csv('data/csv_files/visual_files/test_data_visual.csv').set_index('movie')
+
+visual_avg_dev = pd.read_csv('data/csv_files/visual_files/dev_data_visual_avg.csv').set_index('movie')
+visual_avg_test = pd.read_csv('data/csv_files/visual_files/test_data_visual_avg.csv').set_index('movie')
+
+x = sorted(list(set(data_dev.index.tolist()) - set(meta_dev.index.tolist())))
+y = sorted(list(set(meta_dev.index.tolist()) - set(data_dev.index.tolist())))
+
+for i in range(len(x)):
+    meta_dev = meta_dev.rename(index={y[i]: x[i]})
+    rating_dev = rating_dev.rename(index={y[i]: x[i]})
+    
+x = sorted(list(set(data_test.index.tolist()) - set(meta_test.index.tolist())))
+y = sorted(list(set(meta_test.index.tolist()) - set(data_test.index.tolist())))
+
+for i in range(len(x)):
+    meta_test = meta_test.rename(index={y[i]: x[i]})
+    rating_test = rating_test.rename(index={y[i]: x[i]})
+
+rating_dev_arff = pd.concat([rating_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(rating_dev.index)
+rating_test_arff = pd.concat([rating_test, data_test['goodforairplane']], axis=1, sort=True).reindex(rating_test.index)    
+
+meta_dev_arff = pd.concat([meta_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(meta_dev.index)
+meta_test_arff = pd.concat([meta_test, data_test['goodforairplane']], axis=1, sort=True).reindex(meta_test.index)
+
+meta_visual_dev_arff = pd.concat([meta_dev_arff, visual_dev], axis=1, sort=True).reindex(meta_dev_arff.index)
+meta_visual_test_arff = pd.concat([meta_test_arff, visual_test], axis=1, sort=True).reindex(meta_test_arff.index)
+
+meta_visual_avg_dev_arff = pd.concat([meta_dev_arff, visual_avg_dev], axis=1, sort=True).reindex(meta_dev_arff.index)
+meta_visual_avg_test_arff = pd.concat([meta_test_arff, visual_avg_test], axis=1, sort=True).reindex(meta_test_arff.index)
+
+meta_rating_dev_arff = pd.concat([meta_dev_arff, rating_dev], axis=1, sort=True).reindex(meta_dev_arff.index)
+meta_rating_test_arff = pd.concat([meta_test_arff, rating_test], axis=1, sort=True).reindex(meta_test_arff.index)
+
+vis_dev = pd.concat([visual_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(visual_dev.index)
+vis_dev_avg = pd.concat([visual_avg_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(visual_avg_dev.index)
+vis_test = pd.concat([visual_test, data_test['goodforairplane']], axis=1, sort=True).reindex(visual_test.index)
+vis_test_avg = pd.concat([visual_avg_test, data_test['goodforairplane']], axis=1, sort=True).reindex(visual_avg_test.index)
+
+arff.dump('data/WEKA_files/visual_files/dev_data_vis.arff', vis_dev.values
+      , relation='visual_descriptors'
+      , names=vis_dev.columns)
+
+arff.dump('data/WEKA_files/visual_files/dev_data_vis_avg.arff', vis_dev_avg.values
+      , relation='visual_descriptors'
+      , names=vis_dev_avg.columns)
+
+arff.dump('data/WEKA_files/visual_files/test_data_vis.arff', vis_test.values
+      , relation= 'visual_descriptors'
+      , names=vis_test.columns)
+
+arff.dump('data/WEKA_files/visual_files/test_data_vis_avg.arff', vis_test_avg.values
+      , relation= 'visual_descriptors'
+      , names=vis_test_avg.columns)
+
+
+arff.dump('data/WEKA_files/user_rating_files/dev_data_rating.arff', rating_dev_arff.values
+      , relation='rating'
+      , names=rating_dev_arff.columns)
+
+arff.dump('data/WEKA_files/user_rating_files/test_data_rating.arff', rating_test_arff.values
+      , relation='rating'
+      , names=rating_test_arff.columns)
+      
+arff.dump('data/WEKA_files/metadata_files/dev_data_meta.arff', meta_dev_arff.values
+      , relation='metadata'
+      , names=meta_dev_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_files/test_data_meta.arff', meta_test_arff.values
+      , relation='metadata'
+      , names=meta_test_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_user_rating_files/dev_data_meta_rating.arff', meta_rating_dev_arff.values
+      , relation='metadata_rating'
+      , names=meta_rating_dev_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_user_rating_files/test_data_meta_rating.arff', meta_rating_test_arff.values
+      , relation='metadata_rating'
+      , names=meta_rating_test_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_visual_files/dev_data_meta_visual.arff', meta_visual_dev_arff.values
+      , relation='metadata_visual'
+      , names=meta_visual_dev_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_visual_files/test_data_meta_visual.arff', meta_visual_test_arff.values
+      , relation='metadata_visual'
+      , names=meta_visual_test_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_visual_files/dev_data_meta_visual_avg.arff', meta_visual_avg_dev_arff.values
+      , relation='metadata_visual'
+      , names=meta_visual_avg_dev_arff.columns)
+
+arff.dump('data/WEKA_files/metadata_visual_files/test_data_meta_visual_avg.arff', meta_visual_avg_test_arff.values
+      , relation='metadata_visual'
+      , names=meta_visual_avg_test_arff.columns)
--- visual_table_arff_export.py
+++ visual_table_arff_export.py
@@ -78,25 +78,29 @@ vis_dev_avg = visual_data(data_dev, columns_avg, 'dev')
 vis_test = visual_data(data_test, columns_all, 'test')
 vis_test_avg = visual_data(data_test, columns_avg, 'test')
 
+vis_dev_ = pd.concat([vis_dev, data_dev['goodforairplane']], axis=1, sort=True).reindex(vis_dev.index)
+vis_dev_avg_ = pd.concat([vis_dev_avg, data_dev['goodforairplane']], axis=1, sort=True).reindex(vis_dev_avg.index)
+vis_test_ = pd.concat([vis_test, data_test['goodforairplane']], axis=1, sort=True).reindex(vis_test.index)
+vis_test_avg_ = pd.concat([vis_test_avg, data_test['goodforairplane']], axis=1, sort=True).reindex(vis_test_avg.index)
 
-arff.dump('data/WEKA_files/visual_files/dev_data_vis.arff', vis_dev.values
+arff.dump('data/WEKA_files/visual_files/dev_data_vis.arff', vis_dev_.values
       , relation='visual_descriptors'
-      , names=vis_dev.columns)
+      , names=vis_dev_.columns)
 
-arff.dump('data/WEKA_files/visual_files/dev_data_vis_avg.arff', vis_dev_avg.values
+arff.dump('data/WEKA_files/visual_files/dev_data_vis_avg.arff', vis_dev_avg_.values
       , relation='visual_descriptors'
-      , names=vis_dev_avg.columns)
+      , names=vis_dev_avg_.columns)
 
-arff.dump('data/WEKA_files/visual_files/test_data_vis.arff', vis_test.values
+arff.dump('data/WEKA_files/visual_files/test_data_vis.arff', vis_test_.values
+      , relation= 'visual_descAriptors'
+      , names=vis_test_.columns)
+
+arff.dump('data/WEKA_files/visual_files/test_data_vis_avg.arff', vis_test_avg_.values
       , relation= 'visual_descriptors'
-      , names=vis_test.columns)
+      , names=vis_test_avg_.columns)
 
-arff.dump('data/WEKA_files/visual_files/test_data_vis_avg.arff', vis_test_avg.values
-      , relation= 'visual_descriptors'
-      , names=vis_test_avg.columns)
+vis_dev_.to_csv('data/csv_files/visual_files/dev_data_visual.csv')
+vis_dev_avg_.to_csv('data/csv_files/visual_files/dev_data_visual_avg.csv')
 
-vis_dev.to_csv('data/csv_files/visual_files/dev_data_visual.csv')
-vis_dev_avg.to_csv('data/csv_files/visual_files/dev_data_visual_avg.csv')
-
-vis_test.to_csv('data/csv_files/visual_files/test_data_visual.csv')
-vis_test_avg.to_csv('data/csv_files/visual_files/test_data_visual_avg.csv')
+vis_test_.to_csv('data/csv_files/visual_files/test_data_visual.csv')
+vis_test_avg_.to_csv('data/csv_files/visual_files/test_data_visual_avg.csv')
